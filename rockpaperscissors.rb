6e9ee801a8dec4a7fab56a89ce9b9a3af6c9b3e2
Luke Korth <lukekorth@gmail.com>
2015-01-29 17:14:58 -0600

    making good progress, have it working with a single Player class which will keeps each player's score in a hash.

--- rockpaperscissors.rb
+++ rockpaperscissors.rb
@@ -1,23 +1,40 @@
 require "pry"
 
-P1W = ["rs", "sp", "pr"]
-P2W = ["sr", "ps", "rp"]
-TIE = ["rr", "ss", "pp"]
+class Player
+  def initialize
+    @players = {}
+    @rules = [["rs", "sp", "pr"],  ["sr", "ps", "rp"],  ["rr", "ss", "pp"]]
+  end
+  def add_player(player)
+    @players[player] = 0.0
+    @players 
+  end
   
+  def play_game(player1, player2)  
+    print "#{player1} select move (r, s, or p). "
+    move1 = gets.chomp.downcase
+    print "#{player2} select move (r, s, or p). "
+    move2 = gets.chomp.downcase
 
-
-
-print "Player one select move (r, s, or p). "
-move1 = gets.chomp.downcase
-print "Player two select move (r, s, or p). "
-move2 = gets.chomp.downcase
-
-if P1W.include?("#{move1}#{move2}")
-  puts "Player 1 Wins!"
-elsif P2W.include?("#{move1}#{move2}")
-  puts "Player 2 Wins!"
-else
-  puts "It's a Draw!"
+    if @rules[0].include?("#{move1}#{move2}")
+      puts "#{player1} Wins!"
+      @players["#{player1}"] += 1
+      @players["#{player2}"] -= 1
+    elsif @rules[1].include?("#{move1}#{move2}")
+      puts "#{player2} Wins!"
+      @players["#{player1}"] -= 1
+      @players["#{player2}"] += 1
+    elsif @rules[2].include?("#{move1}#{move2}")
+      puts "It's a Draw!"
+    else
+      puts "Sorry, Invalid Entry!"
+    end
+    @players
+  end
 end
+a = Player.new
+a.add_player("Luke")
+a.add_player("Matthew")
+binding.pry
 
 
